bf9435bcf1e36f52888a0b6abca28ce7ba116735
Gautam Mekkat <Blue9@users.noreply.github.com>
2022-04-25 18:57:21 -0400

    Add place name to discover feed (#15)
    Co-authored-by: Kevin Nizza <kevinnizza1@gmail.com>

--- Jimo/Jimo/Views/Views/Search/Search.swift
+++ Jimo/Jimo/Views/Views/Search/Search.swift
@@ -34,6 +34,31 @@ struct Search: View {
         ProfileScreen(initialUser: user)
     }
     
+    @ViewBuilder
+    func discoverCell(post: Post) -> some View {
+        VStack(alignment: .leading, spacing: 0) {
+            GeometryReader { geometry in
+                URLImage(url: post.imageUrl, thumbnail: true)
+                    .frame(maxWidth: .infinity)
+                    .frame(width: geometry.size.width, height: geometry.size.width)
+            }
+            .aspectRatio(1, contentMode: .fit)
+            .background(Color(post.category))
+            .cornerRadius(2)
+            .padding(.bottom, 5)
+            
+            Text(post.place.name)
+                .font(.system(size: 12))
+                .bold()
+                .lineLimit(1)
+            
+            Text(post.place.regionName ?? "")
+                .font(.system(size: 12))
+                .lineLimit(1)
+        }
+        .padding(.bottom, 10)
+    }
+    
     @ViewBuilder var discoverFeed: some View {
         if !discoverViewModel.initialized {
             ProgressView().padding(.top, 20)
@@ -45,16 +70,9 @@ struct Search: View {
     var discoverFeedLoaded: some View {
         ASCollectionView {
             ASCollectionViewSection(id: 0, data: discoverViewModel.posts) { post, _ in
-                GeometryReader { geometry in
-                    NavigationLink(destination: ViewPost(initialPost: post)) {
-                        URLImage(url: post.imageUrl, thumbnail: true)
-                            .frame(maxWidth: .infinity)
-                            .frame(width: geometry.size.width, height: geometry.size.width)
-                    }
+                NavigationLink(destination: ViewPost(initialPost: post)) {
+                    discoverCell(post: post)
                 }
-                .aspectRatio(1, contentMode: .fit)
-                .background(Color(post.category))
-                .cornerRadius(2)
             }
         }
         .alwaysBounceVertical()
@@ -138,12 +156,5 @@ struct Search: View {
             .trackScreen(.searchTab)
         }
         .navigationViewStyle(StackNavigationViewStyle())
-    }
-}
-
-struct Search_Previews: PreviewProvider {
-    
-    static var previews: some View {
-        Search()
     }
 }
